7b169f803342715a37f96241810137ebfc7ecd6d
sjsutton27 <ssutton101600@gmail.com>
2023-11-06 13:35:46 -0500

    Added details to cart-page

--- src/views/cart-page.vue
+++ src/views/cart-page.vue
@@ -1,9 +1,31 @@
 <template>
-  <h1>Hello Cart</h1>
+  <div class="cart">
+    <ul v-if="cart && cart.length != 0">
+      <li v-for="item in cart" :key="item.id">
+        <p>
+          <span>{{ item.name }}</span> <br />
+          <span>{{ item.price }}</span
+          ><br />
+          <span>{{ item.quantity }}</span>
+          <br />
+        </p>
+      </li>
+    </ul>
+    <h3 v-else>No Items in Cart</h3>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    cart() {
+      return this.$store.getters["cart/getCart"]
+    },
+    total() {
+      return this.$store.getters["cart/getTotal"]
+    }
+  }
+}
 </script>
 
 <style scoped></style>
